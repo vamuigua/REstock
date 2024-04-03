e2c66571bbf6d016daa3dfbc370a31851c153003
Victor Allen <vamuigua@gmail.com>
2024-04-03 12:33:20 +0300

    update(shopping): config models and UI for item categories

---
+++ lib/widget/grocery_list.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import 'package:shopping_list/data/dummy_items.dart';
+
+class GroceryList extends StatelessWidget {
+  const GroceryList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Groceries'),
+      ),
+      body: ListView.builder(
+        itemCount: groceryItems.length,
+        itemBuilder: (ctx, index) {
+          return ListTile(
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: groceryItems[index].category.color,
+            ),
+            title: Text(groceryItems[index].name),
+            trailing: Text(
+              groceryItems[index].quantity.toString(),
+              style: const TextStyle(fontSize: 15.0),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}

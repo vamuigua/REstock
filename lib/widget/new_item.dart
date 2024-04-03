f13219f8a3c81ad44cdcd6d0c33fdfbc17c4ff59
Victor Allen <vamuigua@gmail.com>
2024-04-03 16:11:43 +0300

    update: add form validation logic to name & quantity fields

--- lib/widget/new_item.dart
+++ lib/widget/new_item.dart
@@ -26,10 +26,12 @@ class _NewItemState extends State<NewItem> {
                   label: Text("Name"),
                 ),
                 validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Please enter a name";
+                  if (value == null ||
+                      value.isEmpty ||
+                      value.trim().length <= 1 ||
+                      value.trim().length > 50) {
+                    return "Must be between 1 and 50 characters long";
                   }
-
                   return null;
                 },
               ),
@@ -42,6 +44,15 @@ class _NewItemState extends State<NewItem> {
                         label: Text("Quantity"),
                       ),
                       initialValue: "1",
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            int.tryParse(value) == null ||
+                            int.tryParse(value)! <= 0) {
+                          return "Must be a valid, positive number";
+                        }
+                        return null;
+                      },
                     ),
                   ),
                   const SizedBox(width: 8),
@@ -69,6 +80,20 @@ class _NewItemState extends State<NewItem> {
                   )
                 ],
               ),
+              const SizedBox(height: 12),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text("Reset"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text("Save item"),
+                  ),
+                ],
+              )
             ],
           ),
         ),

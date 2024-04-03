690f9b3c908e14337018989775904f7a73be3bd4
Victor Allen <vamuigua@gmail.com>
2024-04-03 14:53:34 +0300

    UI: add NewItem screen

---
+++ lib/widget/new_item.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class NewItem extends StatefulWidget {
+  const NewItem({super.key});
+
+  @override
+  State<NewItem> createState() => _NewItemState();
+}
+
+class _NewItemState extends State<NewItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add a new item"),
+      ),
+      body: const Text("Form will be here soon..."),
+    );
+  }
+}

76ec6e2bfd8ccc255166be387855719d647722e5
Victor Allen <vamuigua@gmail.com>
2024-04-05 16:55:31 +0300

    WIP feat: add google realtime DB as backend storage

--- lib/widget/new_item.dart
+++ lib/widget/new_item.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
-import 'package:shopping_list/models/grocery_item.dart';
+// import 'package:shopping_list/models/grocery_item.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -19,14 +24,26 @@ class _NewItemState extends State<NewItem> {
   void _saveItem() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
+      final url = Uri.https(
+          "restock-cc312-default-rtdb.asia-southeast1.firebasedatabase.app",
+          "shopping-list.json");
+
+      http.post(url,
+          headers: {"Content-type": "application/json"},
+          body: json.encode({
+            'name': _enteredName,
+            'quantity': _enteredQuantity,
+            'category': _selectedCategory.title,
+          }));
+
       Navigator.of(context).pop(
-        GroceryItem(
-          id: DateTime.now().toString(),
-          name: _enteredName,
-          quantity: _enteredQuantity,
-          category: _selectedCategory,
-        ),
-      );
+          // GroceryItem(
+          //   id: DateTime.now().toString(),
+          //   name: _enteredName,
+          //   quantity: _enteredQuantity,
+          //   category: _selectedCategory,
+          // ),
+          );
     }
   }
 
